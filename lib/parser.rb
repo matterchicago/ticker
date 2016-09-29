c5e482aaea6adb198f8dadebb1d769ac101ba0b6
claytongentry <claytongentry2017@u.northwestern.edu>
2016-09-29 11:05:34 -0500

    use load_file

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,8 +5,7 @@ module Parser
   FILE = "data/entities.yml"
 
   def self.get_data(entity_name)
-    file = File.read(FILE)
-    hash = YAML.load(file)
+    hash = YAML.load_file(FILE)
 
     if hash.keys.include?(entity_name)
       return hash[entity_name]
